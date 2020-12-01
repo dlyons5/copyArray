9e6f26cdfd3cca35d75e1c04e8ea5d8bf6f7de50
dlyons5 <dominic.lyons@patelhs.org>
2020-12-01 13:09:09 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Main {
+ 
+  public static void main(String[] args) {
+  
+  String[] roster1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+   for (String i = 0; i <= 10; i++){
+      System.out.println(roster1[i]);
+   {
+  
+  String[] roster2 = new String[10];
+
+   for (i = 0; i < 10; i++){
+      System.out.println(roster2[i]);    
+  
+        }
+      }
+    }
+  }
+}
