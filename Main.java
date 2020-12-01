27c57ff78d6f694b8274a84ec0379553e4a8a3f4
dlyons5 <dominic.lyons@patelhs.org>
2020-12-01 13:15:04 +0000

    part c: using a for loop and print statement

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 class Main {
 
   public static void main(String[] args) {
-  
+//roster 1  
   int[] roster1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
    for (int i = 0; i <= 10; i++){
       System.out.println(roster1[i]);
    {
-  
+//roster 2   
   String[] roster2 = new String[10];
 
    for (i = 0; i < 10; i++){
