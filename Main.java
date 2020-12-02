beac9f0f48f2b50d90df43ed914adb3cf98a3d28
dlyons5 <dominic.lyons@patelhs.org>
2020-12-02 19:03:59 +0000

    i fixed it !!!!

--- Main.java
+++ Main.java
@@ -1,19 +1,13 @@
 class Main {
 
   public static void main(String[] args) {
-    int[] arr1 = new int[5];
-    int[] arr2 = {44,45,46,47,48};
-
-    int sum;
-    sum = 3;
-
-    arr1[0] = 4;
-    arr1[1] = arr2[1]; 
-
-    for(int index = 2; index < arr1.length; index++)
+    String[] roster1 = new String[10];
+    String[] roster2 = {"Bob","Ben","Jeff","Noah","Elija","Dylan","Gabe","Seth","Matt","Dominic"};
+    
+    for(int index = 0; index < roster1.length; index++)
     {
-      arr1[index] = arr2[index];
-      System.out.println(arr1[index]);
+      roster1[index] = roster2[index];
+      System.out.println(roster1[index]);
     }
   }
 }
