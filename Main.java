581fb534853e96dc152b70a5e0d490eda653854d
dlyons5 <dominic.lyons@patelhs.org>
2020-12-02 18:09:25 +0000

    this is the example in class

--- Main.java
+++ Main.java
@@ -1,25 +1,22 @@
 class Main {
 
   public static void main(String[] args) {
-//roster 1  
-  int[] roster1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int[] arr1 = new int[5];
+    int[] arr2 = {44,45,46,47,48};
 
-   for (int i = 0; i <= 10; i++){
-      System.out.println(roster1[i]);
-   {
-//roster 2   
-  String[] roster2 = new String[10];
- 
-   for (i = 0; i < 10; i++){
-      System.out.println(roster2[i]);    
-  
-  for ( i = 0; i < roster1.length; i++)
-      {
-          System.out.println( roster1[i] );     
-      }          
-        
-    }
-      }
+    int sum;
+    sum = 3;
+
+    arr1[0] = 4;
+    arr1[1] = arr2[1]; 
+
+    for(int index = 2; index < arr1.length; index++)
+    {
+      arr1[index] = arr2[index];
+      System.out.println(arr1[index]);
     }
   }
 }
+
+
+
